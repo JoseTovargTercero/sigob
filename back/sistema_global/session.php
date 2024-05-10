45b555070b9dd37f32c4d85d8a503754bebd656a
José Ricardo Tovarg III <jose.2710.ricardo@gmail.com>
2024-05-10 19:25:53 -0400

    Corrección al archivo session.php
    Se quito el 'session_regenerate_id'

--- back/sistema_global/session.php
+++ back/sistema_global/session.php
@@ -2,10 +2,7 @@
 require_once 'config.php';
 session_start();
 
-if (!isset($_SESSION["u_oficina"])) {
-	session_regenerate_id(true);
-}else {
-    session_start();
+if (!$_SESSION["u_oficina"]) {
 	session_destroy();
 	header("Location: " . constant('URL'));
 }
