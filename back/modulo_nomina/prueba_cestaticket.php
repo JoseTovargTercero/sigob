73de79fc59679c7dd5f5ba47f316d7980342068d
Moises Celis <143418330+moisescelis21@users.noreply.github.com>
2024-05-13 09:46:01 -0400

    Cestaticket

--- back/modulo_nomina/prueba_cestaticket.php
+++ back/modulo_nomina/prueba_cestaticket.php
@@ -2,27 +2,13 @@
 require_once '../sistema_global/conexion.php';
 require_once '../sistema_global/session.php';
 
+
 // URL del servidor
-$url = 'http://localhost/sigob/back/modulo_nomina/registro_empleados.php/';
+$url = 'http://localhost/sigob/back/modulo_nomina/calculo_cestaticket.php';
 
 // Datos a enviar (el objeto JSON)
 $data = array(
-    "nacionalidad" => "1",
-    "Cedula" => 123456789,
-    "cod_empleado" => 441151,
-    "nombres" => "Pedro Pablo",
-    "fecha_ingreso" => "2010/05/02",
-    "otros_años" => 0,
-    "status" => 1,
-    "observacion" => "N/A",
-    "cod_cargo" => "25212",
-    "cargo" => 12,
-    "banco" => "Venezuela",
-    "cuenta_bancaria" => "1002555541124",
-    "hijos" => 3,
-    "instruccion_academica" => 1,
-    "discapacidades" => 0,
-    "becas" => 0,
+    "valor" => "40",
 );
 
 // Convertir el array a formato JSON
