efa72bb58f4b512bfaab8d3077054280307c37d4
Moises Celis <moisescelis21@gmail.com>
2024-05-13 15:00:34 -0400

    Cambios tabulador

--- back/modulo_nomina/info_tabulador.php
+++ back/modulo_nomina/info_tabulador.php
@@ -2,12 +2,11 @@
 require_once '../sistema_global/conexion.php';
 require_once '../sistema_global/session.php';
 
-
 // Obtener el ID de la URL
 $id = $_GET['id'];
 
 // Consulta SQL para obtener los datos de las tablas
-$sql = "SELECT t.id, t.nombre, t.grados, t.pasos, t.aniosPasos, te.grado , te.paso, te.monto
+$sql = "SELECT t.id, t.nombre, t.grados, t.pasos, t.aniosPasos, te.grado, te.paso, te.monto
         FROM tabuladores t
         INNER JOIN tabuladores_estr te ON t.id = te.tabulador_id
         WHERE t.id = $id";
@@ -25,17 +24,20 @@ $datos = array();
 
 if ($result->num_rows > 0) {
     // Llenar el array con los datos obtenidos de la consulta
-    while($row = $result->fetch_assoc()) {
-        $datos[] = array(
+    while ($row = $result->fetch_assoc()) {
+        $tabulador = array(
             "id" => $row["id"],
             "nombre" => $row["nombre"],
             "grados" => $row["grados"],
             "pasos" => $row["pasos"],
             "aniosPasos" => $row["aniosPasos"],
-            "grado" => $row["grado"],
-            "paso" => $row["paso"],
-            "monto" => $row["monto"]
+            "tabulador" => array(
+                "grado" => $row["grado"],
+                "paso" => $row["paso"],
+                "monto" => $row["monto"]
+            )
         );
+        $datos[] = $tabulador;
     }
 } else {
     echo "No se encontraron resultados.";
@@ -45,5 +47,6 @@ if ($result->num_rows > 0) {
 $conexion->close();
 
 // Pasar el array a la vista (puedes utilizar un archivo de vista o imprimir los datos aquí mismo)
-print_r($datos);
+header('Content-Type: application/json');
+echo json_encode($datos);
 ?>
