d20b7235ae88bb8c881b15e1887aaf3a422996ed
Moises Celis <moisescelis21@gmail.com>
2024-05-15 17:19:51 -0400

    cambios en empleados

--- back/modulo_nomina/nom_empleados_registro.php
+++ back/modulo_nomina/nom_empleados_registro.php
@@ -6,14 +6,14 @@ require_once '../sistema_global/conexion.php';
 $data = json_decode(file_get_contents('php://input'), true);
 
 // Construir la consulta SQL para insertar datos
-$sql = "INSERT INTO empleados (nacionalidad, Cedula, cod_empleado, nombres, fecha_ingreso, otros_años, status, observacion, cod_cargo, cargo, banco, cuenta_bancaria, hijos, instruccion_academica, discapacidades, becas, tipo_cuenta, tipo_nomina, id_dependencia)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+$sql = "INSERT INTO empleados (nacionalidad, cedula, cod_empleado, nombres, fecha_ingreso, otros_años, status, observacion, cod_cargo, banco, cuenta_bancaria, hijos, instruccion_academica, discapacidades, tipo_cuenta, tipo_nomina, id_dependencia)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 // Preparar la declaración SQL
 $stmt = $conexion->prepare($sql);
 
 // Vincular parámetros y ejecutar la consulta
-$stmt->bind_param("iisssisssssisisiiii", $data["nacionalidad"], $data["Cedula"], $data["cod_empleado"], $data["nombres"], $data["fecha_ingreso"], $data["otros_años"], $data["status"], $data["observacion"], $data["cod_cargo"], $data["cargo"], $data["banco"], $data["cuenta_bancaria"], $data["hijos"], $data["instruccion_academica"], $data["discapacidades"], $data["becas"], $data["tipo_cuenta"], $data["tipo_nomina"], $data["id_dependencia"]);
+$stmt->bind_param("iisssissssisiiiii", $data["nacionalidad"], $data["cedula"], $data["cod_empleado"], $data["nombres"], $data["fecha_ingreso"], $data["otros_años"], $data["status"], $data["observacion"], $data["cod_cargo"], $data["banco"], $data["cuenta_bancaria"], $data["hijos"], $data["instruccion_academica"], $data["discapacidades"], $data["tipo_cuenta"], $data["tipo_nomina"], $data["id_dependencia"]);
 
 // Ejecutar la consulta preparada
 if ($stmt->execute()) {
