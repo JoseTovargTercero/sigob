4397adbe5a58f51d5a74a6374967fbf55f77d796
José Ricardo Tovarg III <jose.2710.ricardo@gmail.com>
2024-05-29 17:18:47 -0400

    Modulo de creacion de usuarios

--- front/includes/header.php
+++ front/includes/header.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="<?php echo constant('URL') ?>/src/assets/css/style.css" id="main-style-link">
     <link rel="stylesheet" href="<?php echo constant('URL') ?>/src/assets/css/style-preset.css">
     <script src="<?php echo constant('URL') ?>src/assets/js/sweetalert2.all.min.js"></script>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-14K1GBX9FG"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <link rel="stylesheet" crossorigin="anonymous" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
