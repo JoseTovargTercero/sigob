98702eb65972e47ec7c8a42ed20ea0d799d6d25a
José Ricardo Tovarg III <jose.2710.ricardo@gmail.com>
2024-07-15 08:19:42 -0400

    chore: Move employee to past employees table when deleting

--- back/modulo_nomina/nom_empleados_delete.php
+++ back/modulo_nomina/nom_empleados_delete.php
@@ -1,9 +1,49 @@
 <?php
+
 require_once '../sistema_global/conexion.php';
 
 // Verificar si el parámetro 'id' está presente en la URL
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
+
+    // sacar de las nominas al que pertenece 
+    $stmt = mysqli_prepare($conexion, "SELECT *, TIMESTAMPDIFF(YEAR, fecha_ingreso, CURDATE()) + otros_años AS antiguedad_total FROM empleados WHERE id = ?");
+    $stmt->bind_param('s', $id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    if ($result->num_rows > 0) {
+        $empleado = $result->fetch_assoc();
+    }
+    $stmt->close();
+    
+    // insertar en empleados_pasados
+    $stmt = $conexion->prepare("INSERT INTO empleados_pasados (nacionalidad, cedula, nombres, otros_años, status, observacion, cod_cargo, banco, cuenta_bancaria, hijos, instruccion_academica, discapacidades, tipo_nomina, id_dependencia, verificado, correcion, beca, fecha_ingreso) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssssssssssssssss", 
+        $empleado['nacionalidad'], 
+        $empleado['cedula'], 
+        $empleado['nombres'], 
+        $empleado['antiguedad_total'], 
+        $empleado['status'], 
+        $empleado['observacion'], 
+        $empleado['cod_cargo'], 
+        $empleado['banco'], 
+        $empleado['cuenta_bancaria'], 
+        $empleado['hijos'], 
+        $empleado['instruccion_academica'], 
+        $empleado['discapacidades'], 
+        $empleado['tipo_nomina'], 
+        $empleado['id_dependencia'], 
+        $empleado['verificado'], 
+        $empleado['correcion'], 
+        $empleado['beca'], 
+        $empleado['fecha_ingreso']
+    );
+    $stmt->execute();
+    $stmt->close();
+    
+    exit(); // TODO: QUITAR AL FINALIZAR
+
 
     // Preparar la declaración SQL para eliminar el registro
     $sql = "DELETE FROM empleados WHERE id = ?";
