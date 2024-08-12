7c6a258b278773b679d79d0de3d96b174c43c409
Moises Celis <moisescelis21@gmail.com>
2024-08-11 20:56:15 -0400

    Update pdf_files_config.php

--- back/modulo_relaciones_laborales/pdf_files_config.php
+++ back/modulo_relaciones_laborales/pdf_files_config.php
@@ -1,4 +1,4 @@
 <?php
 // Este archivo contiene la URL base para los archivos PDF
-$base_url = "http://localhost/sigob/back/modulo_nomina/";
+$base_url = "http://localhost/sigob/back/modulo_relaciones_laborales/";
 ?>
