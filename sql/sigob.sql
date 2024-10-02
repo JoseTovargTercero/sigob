9a2f82da880a1ff4f5c022fed00ca09d2be5d24c
Moises Celis <143418330+moisescelis21@users.noreply.github.com>
2024-10-02 13:14:08 -0400

    Update sigob.sql

--- sql/sigob.sql
+++ sql/sigob.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-10-2024 a las 18:57:23
+-- Tiempo de generación: 02-10-2024 a las 19:13:30
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 7.4.33
 
@@ -1004,8 +1004,10 @@ CREATE TABLE `distribucion_entes` (
 CREATE TABLE `distribucion_presupuestaria` (
   `id` int(255) NOT NULL,
   `id_partida` int(255) NOT NULL,
+  `monto_inicial` varchar(255) NOT NULL,
   `monto` varchar(255) NOT NULL,
-  `id_ejercicio` int(255) NOT NULL
+  `id_ejercicio` int(255) NOT NULL,
+  `monto_actual` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
