c23792f3a973ba4efb90e1ce5178f719a78b72d5
Moises Celis <moisescelis21@gmail.com>
2024-10-09 22:22:57 -0400

    Update config.php

--- back/sistema_global/config.php
+++ back/sistema_global/config.php
@@ -6,11 +6,18 @@ if ($_SERVER['SERVER_NAME'] == 'localhost') {
     define('URL', 'http://localhost/sigob/');
     define('PASSWORD', "");
     define('USER', 'root');
-  }else {
+} elseif ($_SERVER['SERVER_NAME'] == 'gitcom-ve.com') {
     define('URL', 'https://gitcom-ve.com/sigob/');
     define('PASSWORD', "JH6$.GnJA6eL");
     define('USER', 'sigob_user');
-  }
+} else {
+    $url = 'https://'. $_SERVER['SERVER_NAME'].'/sigob/';
+    define('URL', $url);
+    define('PASSWORD', "JH6$.GnJA6eL");
+    define('USER', 'sigob_user');
+}
+
+  
 
 
 
