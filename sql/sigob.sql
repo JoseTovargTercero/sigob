239ae3fe6b95971e0830e4844894fb8a21ebbdbf
Moises Celis <moisescelis21@gmail.com>
2024-10-14 00:01:21 -0400

    Update sigob.sql

--- sql/sigob.sql
+++ sql/sigob.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-10-2024 a las 05:55:25
+-- Tiempo de generación: 14-10-2024 a las 06:00:51
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 7.4.33
 
