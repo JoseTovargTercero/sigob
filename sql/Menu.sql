c7b1a4f22306075a26609aa0ab0b7ec2f1ec76eb
Jesús Zapata <jesusnbz22@gmail.com>
2024-11-04 18:58:35 -0400

    Opciones menú

---
+++ sql/Menu.sql
@@ -0,0 +1,8 @@
+INSERT INTO `menu` ( `oficina`, `categoria`, `nombre`, `dir`, `icono`) VALUES
+('pl_formulacion', 'Configuracion', 'Gobernación', 'mod_pl_formulacion/form_gobernacion_tabla', null),
+('pl_formulacion', 'Configuracion', 'Directivos', 'mod_pl_formulacion/form_directivos_tabla', null),
+('pl_formulacion', 'Configuracion', 'Contraloría', 'mod_pl_formulacion/form_contraloria_tabla', null),
+('pl_formulacion', 'Configuracion', 'Consejo', 'mod_pl_formulacion/form_consejo_tabla', null),
+('pl_formulacion', 'Configuracion', 'Personas', 'mod_pl_formulacion/form_persona_tabla', null),
+('pl_formulacion', 'Configuracion', 'Titulo 1', 'mod_pl_formulacion/form_titulo1_tabla', null),
+('pl_formulacion', 'Configuracion', 'Descripcion programa', 'mod_pl_formulacion/form_descripcionPrograma_tabla', null);
