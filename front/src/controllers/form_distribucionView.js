ad2828b25d9df03d76d2b7487277b17a80db3ed3
Jesús Zapata <jesusnbz22@gmail.com>
2024-11-12 18:38:39 -0400

    Cerrar card al eliminar distribución

--- front/src/controllers/form_distribucionView.js
+++ front/src/controllers/form_distribucionView.js
@@ -1,97 +1,100 @@
-import { getFormPartidas } from "../api/partidas.js";
-import { eliminarDistribucion } from "../api/pre_distribucion.js";
-import { getSectores } from "../api/sectores.js";
-import { form_distribucion_form_card } from "../components/form_distribucion_form_card.js";
-import { form_distribucion_modificar_form_card } from "../components/form_distribucion_modificar_card.js";
+import { getFormPartidas } from '../api/partidas.js'
+import { eliminarDistribucion } from '../api/pre_distribucion.js'
+import { getSectores } from '../api/sectores.js'
+import { form_distribucion_form_card } from '../components/form_distribucion_form_card.js'
+import { form_distribucion_modificar_form_card } from '../components/form_distribucion_modificar_card.js'
 import {
   ejerciciosLista,
   validarEjercicioActual,
-} from "../components/form_ejerciciosLista.js";
-import { confirmNotification } from "../helpers/helpers.js";
-import { NOTIFICATIONS_TYPES } from "../helpers/types.js";
+} from '../components/form_ejerciciosLista.js'
+import { confirmNotification } from '../helpers/helpers.js'
+import { NOTIFICATIONS_TYPES } from '../helpers/types.js'
 
 import {
   deleteDistribucionRow,
   loadDistribucionTable,
   validateDistribucionTable,
-} from "./form_distribucionTable.js";
-const d = document;
+} from './form_distribucionTable.js'
+const d = document
 
 export const validateDistribucionView = async () => {
-  let btnNewElement = d.getElementById("partida-registrar");
+  let btnNewElement = d.getElementById('partida-registrar')
 
   let ejercicioFiscal = await ejerciciosLista({
-    elementToInsert: "ejercicios-fiscales",
-  });
+    elementToInsert: 'ejercicios-fiscales',
+  })
 
-  console.log(ejercicioFiscal);
+  console.log(ejercicioFiscal)
 
   validateDistribucionTable({
     partidas: ejercicioFiscal ? ejercicioFiscal.distribucion_partidas : [],
-  });
+  })
 
-  d.addEventListener("click", async (e) => {
-    if (e.target.id === "distribucion-registrar") {
+  d.addEventListener('click', async (e) => {
+    if (e.target.id === 'distribucion-registrar') {
       form_distribucion_form_card({
-        elementToInset: "distribucion-view",
+        elementToInset: 'distribucion-view',
         ejercicioFiscal,
         recargarEjercicio: async function () {
           let ejercicioFiscalElement = d.querySelector(
             `[data-ejercicioid="${ejercicioFiscal.id}"]`
-          );
+          )
           ejercicioFiscal = await validarEjercicioActual({
             ejercicioTarget: ejercicioFiscalElement,
-          });
+          })
         },
-      });
+      })
     }
 
     if (e.target.dataset.ejercicioid) {
       // QUITAR CARD SI SE CAMBIA EL AÑO FISCAL
-      let formCard = d.getElementById("distribucion-form-card");
-      if (formCard) formCard.remove();
+      let formCard = d.getElementById('distribucion-form-card')
+      if (formCard) formCard.remove()
 
       ejercicioFiscal = await validarEjercicioActual({
         ejercicioTarget: e.target,
-      });
+      })
 
-      loadDistribucionTable(ejercicioFiscal.distribucion_partidas);
+      loadDistribucionTable(ejercicioFiscal.distribucion_partidas)
     }
     if (e.target.dataset.editarid) {
-      let partidas = await getFormPartidas();
-      let sectores = await getSectores();
+      let partidas = await getFormPartidas()
+      let sectores = await getSectores()
 
-      console.log(partidas, ejercicioFiscal.partidas);
+      console.log(partidas, ejercicioFiscal.partidas)
       form_distribucion_modificar_form_card({
-        elementToInset: "distribucion-view",
+        elementToInset: 'distribucion-view',
         partidas: partidas.fullInfo,
         sectores: sectores.fullInfo,
         ejercicioFiscal: ejercicioFiscal,
-      });
+      })
     }
 
     if (e.target.dataset.eliminarid) {
       let ejercicioFiscalElement = d.querySelector(
         `[data-ejercicioid="${ejercicioFiscal.id}"]`
-      );
+      )
 
       confirmNotification({
         type: NOTIFICATIONS_TYPES.delete,
-        message: "Se eliminará la distribución<br> <b>¿Está seguro?</b>",
+        message: 'Se eliminará la distribución<br> <b>¿Está seguro?</b>',
         successFunction: async function () {
-          let res = await eliminarDistribucion(e.target.dataset.eliminarid);
+          let res = await eliminarDistribucion(e.target.dataset.eliminarid)
           if (res.success) {
-            let row = e.target.closest("tr");
-            deleteDistribucionRow({ row });
+            let row = e.target.closest('tr')
+            deleteDistribucionRow({ row })
             ejercicioFiscal = await validarEjercicioActual({
               ejercicioTarget: ejercicioFiscalElement,
-            });
+            })
+
+            let formCard = d.getElementById('distribucion-form-card')
+            if (formCard) formCard.remove()
           }
         },
-      });
+      })
     }
-  });
-};
+  })
+}
 
 // function validateEditButtons() {
 //   d.getElementById('partida-registrar').removeAttribute('disabled')
