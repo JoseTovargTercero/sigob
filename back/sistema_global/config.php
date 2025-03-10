b9ebd2b5b61c309698580883cd2a476e0e013284
Moises Celis <143418330+moisescelis21@users.noreply.github.com>
2025-03-10 11:33:29 -0400

    Update config.php

--- back/sistema_global/config.php
+++ back/sistema_global/config.php
@@ -3,18 +3,27 @@
 
 
 if ($_SERVER['SERVER_NAME'] == 'localhost') {
+    define('HOST', 'localhost');
+    define('DB', 'sigob');
+    define('CHARSET', 'utf8mb4');
     define('URL', 'http://localhost/sigob/');
     define('PASSWORD', "");
     define('USER', 'root');
 } elseif ($_SERVER['SERVER_NAME'] == 'gitcom-ve.com') {
+    define('HOST', 'sigob.net');
+define('DB', 'sigobnet_sigob');
+define('CHARSET', 'utf8mb4');
     define('URL', 'https://gitcom-ve.com/sigob/');
     define('PASSWORD', "JH6$.GnJA6eL");
     define('USER', 'sigob_user');
 } else {
-    $url = 'http://' . $_SERVER['SERVER_NAME'] . '/sigob/';
+    $url = 'http://' . $_SERVER['SERVER_NAME'] . '/';
+    define('HOST', 'sigob.net');
+    define('DB', 'sigobnet_sigob');
+    define('CHARSET', 'utf8mb4');
     define('URL', $url);
-    define('PASSWORD', "");
-    define('USER', 'root');
+    define('PASSWORD', "]n^VmqjqCD1k");
+    define('USER', 'sigobnet_userroot');
 }
 
 
@@ -22,6 +31,5 @@ if ($_SERVER['SERVER_NAME'] == 'localhost') {
 
 
 
-define('HOST', 'localhost');
-define('DB', 'sigob');
-define('CHARSET', 'utf8mb4');
+
+
