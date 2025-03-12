ca39371757133c68f96f4a18f6a693da52cf7d23
Moises Celis <moisescelis21@gmail.com>
2025-03-11 22:50:38 -0400

    Update pre_reportes.php

--- back/modulo_ejecucion_presupuestaria/pre_reportes.php
+++ back/modulo_ejecucion_presupuestaria/pre_reportes.php
@@ -60,7 +60,10 @@ $pdf_files = [];
 $url_pdf = "{$base_url}pre_pdf_$tipo.php?id_ejercicio=" . $id_ejercicio;
 if ($tipo == "compromiso") {
     if ($tipo_fecha == "mensual") {
-        $fecha += 1;
+        if ($tipo_tabla == "gastos") {
+            $fecha += 1;
+        }
+        
     }
     $url_pdf = "{$base_url}pre_compromisos_reporte.php?id_ejercicio=$id_ejercicio&tipo=$tipo_tabla&tipo_fecha=$tipo_fecha&fecha=$fecha";
 }
